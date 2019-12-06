24deda8d7d2d31c29b45084f2f7541bd6cd060c3
nightlyasp3ct <41391415+nightlyasp3ct@users.noreply.github.com>
2019-12-06 16:57:58 -0500

    lines 90-91

--- main.py
+++ main.py
@@ -87,7 +87,8 @@ async def on_member_join(member: discord.Member):
         response = discord.Embed(title="Welcome!", description=quip, color=0xa20606)
 
     response.set_author(name="TryHackMe",icon_url="https://tryhackme.com/img/THMlogo.png")
-    response.set_thumbnail(url="https://cdn.discordapp.com/icons/521382216299839518/c0c7e9f1e258dd6d030fde8823bf8657.webp")
+    # The thumbnail here returns a 404 
+    response.set_thumbnail(url="insert_thumbnail")
     response.add_field(name="Hey there!", value=member.mention + ", Welcome to the server!\nIf you need help with a room, ask in #rooms-help.\n\n You can also sync your THM rank on the discord! Use !verify in #bot-commands for more information!")
     
     if member not None:
