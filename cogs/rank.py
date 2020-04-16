50b4281f6f5af0c063f422fe60b1a0e397831567
Aiko <53955553+aiko0@users.noreply.github.com>
2020-04-16 22:36:05 +1000

    Gives indicator that leaderboard is being created

--- cogs/rank.py
+++ cogs/rank.py
@@ -110,132 +110,146 @@ class Rank(commands.Cog,name="Leaderboard Commands"):
 
         @commands.command(description="Prints the leaderboard.")
         async def leaderboard(self,ctx,*,page: int=1):
-                #image_gen(page)
-                print("Generating leaderboard...\n")
+		#Starts typing indicator
+		async with ctx.channel.typing():
+			#image_gen(page)
+			#Logs and prints starting message
+			print("Generating leaderboard...")
+			message = await ctx.send("Generating leaderboard...")
 
-                # Adds a white flag reaction to the user's command.
-                emoji = "\N{Waving White Flag}"
-                await ctx.message.add_reaction(emoji)
+			# Adds a white flag reaction to the user's command.
+			#emoji = "\N{Waving White Flag}"
+			#await ctx.message.add_reaction(emoji)
 
-                # New image.
-                img = Image.new('RGB', (1000, 1000), color=gray)
+			# New image.
+			img = Image.new('RGB', (1000, 1000), color=gray)
 
-                # Adding logo.
-                logo = Image.open("images/THMlogo.png")
-                logo.thumbnail((logo.size[0]/4, logo.size[1]/4), Image.ANTIALIAS)
-                img.paste(logo, (775, 15), logo)
-                d = ImageDraw.Draw(img)
+			# Adding logo.
+			logo = Image.open("images/THMlogo.png")
+			logo.thumbnail((logo.size[0]/4, logo.size[1]/4), Image.ANTIALIAS)
+			img.paste(logo, (775, 15), logo)
+			d = ImageDraw.Draw(img)
 
-                # Adding texts. (leaderboard, mady with, invite link..)
-                d.text((12, 20), "Leaderboard:", font=font1, fill=white)
-                d.text((865-len(str(page))*25, 920), "Page {}".format(page), font=font2, fill=white)
-                d.text((12, 925), "Made with love by the TryHackMe Discord Bot", font=font3, fill=white)
-                d.text((12, 955), "discord.gg/GzVZtGX", font=font3, fill=white)
+			# Adding texts. (leaderboard, mady with, invite link..)
+			d.text((12, 20), "Leaderboard:", font=font1, fill=white)
+			d.text((865-len(str(page))*25, 920), "Page {}".format(page), font=font2, fill=white)
+			d.text((12, 925), "Made with love by the TryHackMe Discord Bot", font=font3, fill=white)
+			d.text((12, 955), "discord.gg/GzVZtGX", font=font3, fill=white)
 
-                # Masking for the users.
-                size = (128, 128)
-                mask = Image.new("L", size, 0)
-                mask_draw = ImageDraw.Draw(mask)
-                mask_draw.ellipse((0, 0) + size, fill=255)
+			# Masking for the users.
+			size = (128, 128)
+			mask = Image.new("L", size, 0)
+			mask_draw = ImageDraw.Draw(mask)
+			mask_draw.ellipse((0, 0) + size, fill=255)
 
-                # Retrieving the users, points and avatar.
-                avatars = getAvatars(page, "topUsers")
-                usernames = getUsernames(page, "topUsers")
-                points = getPoints(page, "topUsers")
+			# Retrieving the users, points and avatar.
+			avatars = getAvatars(page, "topUsers")
+			usernames = getUsernames(page, "topUsers")
+			points = getPoints(page, "topUsers")
 
-                base = 150
-                index = 0
-                pos = (8, base)
-                edge = 975
+			base = 150
+			index = 0
+			pos = (8, base)
+			edge = 975
 
-                #Adding each user.
-                for x in usernames:
-                        response = requests.get(avatars[index])
-                        
-                        # Saving temp image, then adding it to the rest.
-                        temp_img = Image.open(BytesIO(response.content))
-                        temp_img.save('images/temp{}.png'.format(index))
-                        output = ImageOps.fit(temp_img, mask.size, centering=(0.5, 0.5))
-                        img.paste(output, (83, base), mask)
+			#Adding each user.
+			for x in usernames:
+				response = requests.get(avatars[index])
 
-                        # Adding the texts.
-                        d.text((222, base+32), "{}".format(usernames[index]), font=font2, fill=white)
-                        d.text((8, base+32), "{}.".format(index+1+(page-1)*5), font=font2, fill=white)
+				# Saving temp image, then adding it to the rest.
+				temp_img = Image.open(BytesIO(response.content))
+				temp_img.save('images/temp{}.png'.format(index))
+				output = ImageOps.fit(temp_img, mask.size, centering=(0.5, 0.5))
+				img.paste(output, (83, base), mask)
 
-                        d.text((970-(len(str(points[index]))*25), base+32), str(points[index]), font=font2, fill=green)
-                        base += 160
-                        pos = (8, base)
-                        index += 1
+				# Adding the texts.
+				d.text((222, base+32), "{}".format(usernames[index]), font=font2, fill=white)
+				d.text((8, base+32), "{}.".format(index+1+(page-1)*5), font=font2, fill=white)
 
-                # Finalizing, and sending.
-                large_img = img.resize((img.size[0]*4, img.size[1]*4))
-                large_img.save('images/leaderboard_output.png')
-                await ctx.send(file=discord.File('images/leaderboard_output.png'))
+				d.text((970-(len(str(points[index]))*25), base+32), str(points[index]), font=font2, fill=green)
+				base += 160
+				pos = (8, base)
+				index += 1
+
+			# Finalizing, and sending.
+			large_img = img.resize((img.size[0]*4, img.size[1]*4))
+			large_img.save('images/leaderboard_output.png')
+			
+			#Edits starting message with the file
+			await message.edit(content=None,file=discord.File('images/leaderboard_output.png'))
             
 
         @commands.command(description="Prints this month's leaderboard.")
         async def monthly(self,ctx,*,page: int=1):
-                #image_gen(page)
-                print("Generating leaderboard...\n")
+		#Starts typing indicator
+		async with ctx.channel.typing():
 
-                # Adds a white flag reaction to the user's command.
-                emoji = "\N{Waving White Flag}"
-                await ctx.message.add_reaction(emoji)
+			#image_gen(page)
+			#Logs and prints starting message
+			print("Generating leaderboard...")
+			message = await ctx.send("Generating leaderboard...")
+			
 
-                # New image.
-                img = Image.new('RGB', (1000, 1000), color=gray)
+			# Adds a white flag reaction to the user's command.
+			#emoji = "\N{Waving White Flag}"
+			#await ctx.message.add_reaction(emoji)
 
-                # Adding logo.
-                logo = Image.open("images/THMlogo.png")
-                logo.thumbnail((logo.size[0]/4, logo.size[1]/4), Image.ANTIALIAS)
-                img.paste(logo, (775, 15), logo)
-                d = ImageDraw.Draw(img)
+			# New image.
+			img = Image.new('RGB', (1000, 1000), color=gray)
 
-                # Adding texts. (leaderboard, mady with, invite link..)
-                d.text((12, 20), "Monthly Leaderboard:", font=font1, fill=white)
-                d.text((865-len(str(page))*25, 920), "Page {}".format(page), font=font2, fill=white)
-                d.text((12, 925), "Made with love by the TryHackMe Discord Bot", font=font3, fill=white)
-                d.text((12, 955), "discord.gg/GzVZtGX", font=font3, fill=white)
+			# Adding logo.
+			logo = Image.open("images/THMlogo.png")
+			logo.thumbnail((logo.size[0]/4, logo.size[1]/4), Image.ANTIALIAS)
+			img.paste(logo, (775, 15), logo)
+			d = ImageDraw.Draw(img)
 
-                # Masking for the users.
-                size = (128, 128)
-                mask = Image.new("L", size, 0)
-                mask_draw = ImageDraw.Draw(mask)
-                mask_draw.ellipse((0, 0) + size, fill=255)
+			# Adding texts. (leaderboard, mady with, invite link..)
+			d.text((12, 20), "Monthly Leaderboard:", font=font1, fill=white)
+			d.text((865-len(str(page))*25, 920), "Page {}".format(page), font=font2, fill=white)
+			d.text((12, 925), "Made with love by the TryHackMe Discord Bot", font=font3, fill=white)
+			d.text((12, 955), "discord.gg/GzVZtGX", font=font3, fill=white)
 
-                # Retrieving the users, points and avatar.
-                avatars = getAvatars(page, "topUsersMonthly")
-                usernames = getUsernames(page, "topUsersMonthly")
-                points = getPoints(page, "topUsersMonthly")
+			# Masking for the users.
+			size = (128, 128)
+			mask = Image.new("L", size, 0)
+			mask_draw = ImageDraw.Draw(mask)
+			mask_draw.ellipse((0, 0) + size, fill=255)
 
-                base = 150
-                index = 0
-                pos = (8, base)
-                edge = 975
+			# Retrieving the users, points and avatar.
+			avatars = getAvatars(page, "topUsersMonthly")
+			usernames = getUsernames(page, "topUsersMonthly")
+			points = getPoints(page, "topUsersMonthly")
 
-                #Adding each user.
-                for x in usernames:
-                        response = requests.get(avatars[index])
-                        
-                        # Saving temp image, then adding it to the rest.
-                        temp_img = Image.open(BytesIO(response.content))
-                        temp_img.save('images/temp{}.png'.format(index))
-                        output = ImageOps.fit(temp_img, mask.size, centering=(0.5, 0.5))
-                        img.paste(output, (83, base), mask)
+			base = 150
+			index = 0
+			pos = (8, base)
+			edge = 975
 
-                        # Adding the texts.
-                        d.text((222, base+32), "{}".format(usernames[index]), font=font2, fill=white)
-                        d.text((8, base+32), "{}.".format(index+1+(page-1)*5), font=font2, fill=white)
+			#Adding each user.
+			for x in usernames:
+				response = requests.get(avatars[index])
 
-                        d.text((970-(len(str(points[index]))*25), base+32), str(points[index]), font=font2, fill=green)
-                        base += 160
-                        pos = (8, base)
-                        index += 1
+				# Saving temp image, then adding it to the rest.
+				temp_img = Image.open(BytesIO(response.content))
+				temp_img.save('images/temp{}.png'.format(index))
+				output = ImageOps.fit(temp_img, mask.size, centering=(0.5, 0.5))
+				img.paste(output, (83, base), mask)
 
-                # Finalizing, and sending.
-                large_img = img.resize((img.size[0]*4, img.size[1]*4))
-                large_img.save('images/leaderboard_output.png')
-                await ctx.send(file=discord.File('images/leaderboard_output.png'))
+				# Adding the texts.
+				d.text((222, base+32), "{}".format(usernames[index]), font=font2, fill=white)
+				d.text((8, base+32), "{}.".format(index+1+(page-1)*5), font=font2, fill=white)
+
+				d.text((970-(len(str(points[index]))*25), base+32), str(points[index]), font=font2, fill=green)
+				base += 160
+				pos = (8, base)
+				index += 1
+
+			# Finalizing, and sending.
+			large_img = img.resize((img.size[0]*4, img.size[1]*4))
+			large_img.save('images/leaderboard_output.png')
+			
+			#Edits starting message with the file
+			await message.edit(content=None, file=discord.File('images/leaderboard_output.png'))
             
 
 def setup(bot):
