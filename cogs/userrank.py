a9095e405ac0569764094782f45807d54c7b6e39
Szymon Borecki <szymex73@gmail.com>
2020-04-21 07:31:13 +0200

    Minor cleanup and reformatting of userrank.py

--- cogs/userrank.py
+++ cogs/userrank.py
@@ -1,10 +1,4 @@
-import discord
-import aiohttp
-import asyncio
-import json
 from discord.ext import commands
-import requests
-from requests.exceptions import HTTPError
 import random
 
 from libs.embedmaker import officialEmbed
@@ -35,28 +29,30 @@ quotes = [
     "Satoshi Nakamoto",
     "Mining Bitcoin...",
     "Configuring neural network"
-    ]
+]
+
 
 def getMoto():
     return quotes[random.randint(0, len(quotes) - 1)]
 
 
 def sanitize_check(data):
-    chars = ["/",";","-",">","<",":","`","'\"","|"]
+    chars = ["/", ";", "-", ">", "<", ":", "`", "'\"", "|"]
     if any((c in chars) for c in data):
         return True
     else:
         return False
 
 
-class Userrank(commands.Cog,name="Rank Commands"):
-    def __init__(self,bot):
+class Userrank(commands.Cog, name="Rank Commands"):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command(description="Get a THM member's rank.", usage="{username}")
-    async def rank(self,ctx,*,user):
-        if sanitize_check(user) == True:
-            await ctx.send("Sorry, the characters you have entered are blacklisted, instead of trying anything here, try some rooms.")
+    async def rank(self, ctx, *, user):
+        if sanitize_check(user):
+            await ctx.send(
+                "Sorry, the characters you have entered are blacklisted, instead of trying anything here, try some rooms.")
         else:
             try:
                 user_data = thm.getUserData(user)
@@ -66,14 +62,14 @@ class Userrank(commands.Cog,name="Rank Commands"):
                     quip = "*{}*".format(quip)
 
                     response = officialEmbed("!rank", quip, 0x148f77)
-                    
+
                     response.set_thumbnail(url=user_data['avatar'])
-                    response.add_field(name="Username:", value=user_data['avatar'],   inline=True)
-                    response.add_field(name="Rank:",     value=user_data['userRank'],   inline=True)
-                    response.add_field(name="Points:",   value=user_data['points'], inline=True)
-                    
+                    response.add_field(name="Username:", value=user_data['avatar'], inline=True)
+                    response.add_field(name="Rank:", value=user_data['userRank'], inline=True)
+                    response.add_field(name="Points:", value=user_data['points'], inline=True)
+
                     sub = thm.isSubscribed(user)
-                    
+
                     response.add_field(name="Subscribed?", value=sub, inline=True)
                 else:
                     quip = getMoto()
@@ -81,15 +77,16 @@ class Userrank(commands.Cog,name="Rank Commands"):
 
                     response = officialEmbed("!rank", quip, 0xdc143c)
 
-                    userImg = "https://tryhackme.com/img/favicon.png"
-                    
-                    response.set_thumbnail(url=userImg)
+                    user_img = "https://tryhackme.com/img/favicon.png"
+
+                    response.set_thumbnail(url=user_img)
                     response.add_field(name="Username:", value=user, inline=True)
                     response.add_field(name="Rank:", value="**Error: Username Not Found!**", inline=True)
-                            
+
                 await ctx.send(embed=response)
             except:
                 await ctx.send("**An issue has occured.**")
-        
+
+
 def setup(bot):
-	bot.add_cog(Userrank(bot))
+    bot.add_cog(Userrank(bot))
